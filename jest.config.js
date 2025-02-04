f83daada15eb749588854b1a4438e9773a564c89
theorlan2 <theorlan2@gmail.com>
2025-02-04 08:51:12 -0400

    feat: add playwright to create E2E tests.

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["@testing-library/jest-dom", "<rootDir>/jest.setup.js"], // <= setup file here
   testEnvironment: "jest-environment-jsdom",
+  modulePathIgnorePatterns: ["testsE2E/*"],
 };
 
 module.exports = createJestConfig(customJestConfig);
